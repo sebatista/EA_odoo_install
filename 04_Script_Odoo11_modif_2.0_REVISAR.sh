ea1deaa796bb470733e69f5e57ac568ae67a0da9
Sebas <sebatista@yahoo.com>
2021-03-31 23:42:31 -0300

    Update 04_Script_Odoo11_modif_2.0_REVISAR.sh
    Se agrega la actualización del repositorio del core de Odoo.

--- 04_Script_Odoo11_modif_2.0_REVISAR.sh
+++ 04_Script_Odoo11_modif_2.0_REVISAR.sh
@@ -1,14 +1,21 @@
 #CORTAR EL SCRIPT PONERLO EN OTRO LADO
-#sudo su - odoo -s /bin/bash
-#git clone --depth 1 --branch 11.0 --single-branch https://github.com/odoo/odoo odoo-server/
-#sudo pip3 install -r odoo-server/requirements.txt
-#sudo pip3 install -r odoo-server/doc/requirements.txt
-#exit
+sudo su - odoo -s /bin/bash
+git clone --depth 1 --branch 11.0 --single-branch https://github.com/odoo/odoo odoo-server/
+sudo pip3 install -r odoo-server/requirements.txt
+sudo pip3 install -r odoo-server/doc/requirements.txt
+exit
+
+
+#Actualización de Repositorio de Odoo.
+sudo su - odoo -s /bin/bash
+cd /opt/odoo/odoo-server/
+sudo git pull
+exit
 
 #Lo anterior se intenta reemplazar por:
-sudo su - odoo -s /bin/bash -c "git clone --depth 1 --branch 11.0 --single-branch https://github.com/odoo/odoo odoo-server/"
-sudo su - odoo -s /bin/bash -c "pip3 install -r odoo-server/requirements.txt"
-sudo su - odoo -s /bin/bash -c "pip3 install -r odoo-server/doc/requirements.txt"
+#sudo su - odoo -s /bin/bash -c "git clone --depth 1 --branch 11.0 --single-branch https://github.com/odoo/odoo odoo-server/"
+#sudo su - odoo -s /bin/bash -c "pip3 install -r odoo-server/requirements.txt"
+#sudo su - odoo -s /bin/bash -c "pip3 install -r odoo-server/doc/requirements.txt"
 
 #REVISAR 2 Paquetes tiran error de permisos
 # Creo que muchos otros
@@ -17,5 +24,5 @@ sudo su - odoo -s /bin/bash -c "pip3 install -r odoo-server/doc/requirements.txt
 
 #ACTUALIZACIÓN DE BASE DE DATOS Y MÓDULOS
 # sudo su - odoo -s /bin/bash
-# cd /odoo-server/
+# cd /opt/odoo/odoo-server/
 # ./odoo-bin -c /etc/odoo-server.conf --update all
